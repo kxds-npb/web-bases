27ab6e8f388bb5baa24823caa7f0e8de56d65e23
IT-WIBRC <wibrc.se.jc@gmail.com>
2026-01-07 13:49:48 +0100

    course: JsDoc and how to use the  tag in HTML to resolve a specific problem and add documentation to some files

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -5,11 +5,22 @@ document.querySelector("form").addEventListener("submit", (event) => {
 const studentList = [];
 const studentName = document.querySelector("input");
 const addBtn = document.querySelector("button");
+const templateItem = document.getElementById("item");
 
+/**
+ * Retourne un identifiant unique
+ * @returns {string}
+ */
 const generateUID = () => {
   return crypto.randomUUID();
 };
 
+/**
+ * Supprimer un etudiant de la liste eu utilisant l'identifiant de l'etudiant
+ * @param {HTMLUListElement} listToRemoveFrom
+ * @param {string} studentId
+ * @returns {void}
+ */
 function deleteFromListById(listToRemoveFrom, studentId) {
   const studentToDeleteIndex = studentList.findIndex(
     (student) => student.id === studentId
@@ -21,51 +32,77 @@ function deleteFromListById(listToRemoveFrom, studentId) {
   listToRemoveFrom.querySelector(`[data-id="${studentId}"]`).remove();
 }
 
-function addDeleteButtonTo(element, label) {
-  const deleteButton = document.createElement("button");
-  deleteButton.className = "delete-btn";
-  deleteButton.textContent = "❌";
-  deleteButton.ariaLabel = label;
-  deleteButton.title = label;
-  element.appendChild(deleteButton);
+/**
+ * Avoir un clone unique du template pour l'affichage dans la liste
+ * @returns {HTMLLIElement} un item du DOM
+ */
+const getItemCloned = () => {
+  return templateItem.content.cloneNode(true).querySelector("li");
+};
+
+/**
+ * Inserer un etudiant dans la liste des etudiants visible dans le DOM
+ * @param {Student} student un object ayant la structure d'un etudiant
+ * @param {HTMLUListElement} target
+ * @returns {void}
+ */
+function insertStudentToDOMList(student, target) {
+  const item = getItemCloned();
+  item.querySelector("p").innerText = student.name;
+  item.dataset.id = student.id;
+
+  const label = `Enlever l'etudiant ${student.name} de la liste`;
+  const deleteButton = item.querySelector("button");
+  deleteButton.setAttribute("aria-label", label);
+  deleteButton.setAttribute("title", label);
+  deleteButton.onclick = () => deleteFromListById(target, student.id);
+
+  target.appendChild(item);
+  item.scrollIntoView(); // va scroller a l'emplacement ou l'element est place dans la page pour qu'il soit visible
 }
 
-function insertStudentToList(student, target) {
-  const li = document.createElement("li");
-  li.className = "student";
-  li.textContent = student.name;
-  li.dataset.id = student.id;
-
-  addDeleteButtonTo(li, `Enlever l'etudiant ${student.name} de la liste`);
-
-  li.querySelector("button").addEventListener("click", () => {
-    deleteFromListById(target, student.id);
-  });
-
-  target.appendChild(li);
-  li.scrollIntoView();
-}
-
+/**
+ * Rafraichir la liste
+ * @returns {void}
+ */
 function refreshList() {
   const list = document.querySelector(".list");
   list.replaceChildren();
-  studentList.forEach((student) => insertStudentToList(student, list));
+  studentList.forEach((student) => insertStudentToDOMList(student, list));
 }
 
+/**
+ * Trier la liste par ordre alphabetique (A a Z)
+ * @returns { void }
+ */
 function sortStudentList() {
   studentList.sort((studentA, studentB) =>
     studentA.name.toLowerCase().localeCompare(studentB.name.toLowerCase())
   );
 }
 
+/**
+ * Mettre la premiere lettre du nom en majuscule
+ * @param {string} name mot a tranformer
+ * @returns {string}
+ */
 function capitalize(name) {
   return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 }
 
+/**
+ * Effacer le contenue de l'input
+ * @returns {void}
+ */
 function clearField() {
   studentName.value = "";
 }
 
+/**
+ * Ajouter l'etudiant dans la liste des noms
+ * @param {string} nameToAdd nom de l'etudiant a ajouter dans la liste
+ * @returns {void}
+ */
 function addToStudentList(nameToAdd) {
   const newStudent = {
     id: generateUID(),
@@ -75,6 +112,11 @@ function addToStudentList(nameToAdd) {
   studentList.push(newStudent);
 }
 
+/**
+ * Manager le processus d'ajout un etudiant
+ * @param {string} studentName nom de l'etudiant
+ * @returns {void}
+ */
 function processAddition(studentName) {
   const hasThisStudentAlready = studentList.find(
     (student) =>
